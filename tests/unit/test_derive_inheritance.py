8a26e9916be1544080a4ece04fc57d7cb3029b54
Kirill Tribunsky <tribunsky.kir@yandex.ru>
2023-05-12 12:48:34 +0400

    fix: KeyError: '__annotations__'

---
+++ tests/unit/test_derive_inheritance.py
@@ -0,0 +1,75 @@
+import sys
+import json
+from typing import Optional
+
+from pydantic import Field, BaseModel
+
+from wunderkafka.serdes.avromodel import derive
+
+if sys.version_info > (3, 7):
+    class Parent(BaseModel):
+        volume: float = Field(description='...')
+        weight: Optional[float] = Field(description='...')
+
+    class Child(Parent):
+        ts: int = Field(description='...')
+
+
+    class Grandson(Child):
+        ...
+
+
+    def test_dataclass() -> None:
+        schema = derive(Grandson, topic='test_data_1')
+        assert json.loads(schema) == {
+            'type': 'record',
+            'name': 'test_data_1_value',
+            'fields': [
+                {
+                    'name': 'volume',
+                    'type': 'double',
+                },
+                {
+                    'name': 'weight',
+                    'type': ['double', 'null'],
+                },
+                {
+                    'name': 'ts',
+                    'type': 'long',
+                },
+            ],
+        }
+
+if sys.version_info > (3, 10):
+    class Parent(BaseModel):
+        volume: float = Field(description='...')
+        weight: float | None = Field(description='...')
+
+
+    class Child(Parent):
+        ts: int = Field(description='...')
+
+
+    class Grandson(Child):
+        ...
+
+    def test_dataclass_pipe_annotation() -> None:
+        schema = derive(Grandson, topic='test_data_1')
+        assert json.loads(schema) == {
+            'type': 'record',
+            'name': 'test_data_1_value',
+            'fields': [
+                {
+                    'name': 'volume',
+                    'type': 'double',
+                },
+                {
+                    'name': 'weight',
+                    'type': ['double', 'null'],
+                },
+                {
+                    'name': 'ts',
+                    'type': 'long',
+                },
+            ],
+        }
