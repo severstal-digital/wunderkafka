9e17233ec89ff0b11a10958d17b4a784ecea699a
Alex <rabbit_1399@icloud.com>
2023-09-27 15:25:39 +0300

    feat: added multiple keytabs to be used at the same time

---
+++ tests/unit/test_krb_watchdog.py
@@ -0,0 +1,13 @@
+from unittest.mock import Mock
+
+from pytest import MonkeyPatch
+
+from wunderkafka.hotfixes.watchdog import KrbWatchDog, KinitParams
+
+
+def test_krb_watchdog(monkeypatch: MonkeyPatch) -> None:
+    params = [KinitParams(user=f'user_{i}', realm='realm', keytab=f'keytab{i}', cmd='cmd') for i in range(10)]
+    monkeypatch.setattr('wunderkafka.hotfixes.watchdog.KrbWatchDog._ensure_started', Mock())
+    for i, p in enumerate(params, start=1):
+        thread = KrbWatchDog(p)
+        assert len(thread.__dict__['_KrbWatchDog__kinit_params']) == i
--- tests/unit/test_parse_kinit.py
+++ tests/unit/test_parse_kinit.py
@@ -1,16 +1,15 @@
-from wunderkafka.hotfixes.watchdog import parse_kinit
+import pytest
+
+from wunderkafka.hotfixes.watchdog import parse_kinit, KinitParams
 
 USER = 'my.user'
 REALM = 'MYDOMAIN.COM'
 
 
-def test_relative_regular() -> None:
-    keytab = '{0}.keytab'.format(USER)
+@pytest.mark.parametrize(
+    'keytab',
+    ['{0}.keytab'.format(USER), '/home/{0}@{1}/{0}.keytab'.format(USER, REALM.lower())]
+)
+def test_parse_kinit(keytab: str) -> None:
     kinit_cmd = 'kinit {0}@{1} -k -t {2}'.format(USER, REALM, keytab)
-    assert parse_kinit(kinit_cmd) == (USER, REALM, keytab)
-
-
-def test_absolute_with_domain() -> None:
-    keytab = '/home/{0}@{1}/{0}.keytab'.format(USER, REALM.lower())
-    kinit_cmd = 'kinit {0}@{1} -k -t {2}'.format(USER, REALM, keytab)
-    assert parse_kinit(kinit_cmd) == (USER, REALM, keytab)
+    assert parse_kinit(kinit_cmd) == KinitParams(user=USER, realm=REALM, keytab=keytab, cmd=kinit_cmd)
