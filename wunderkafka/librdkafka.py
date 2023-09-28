6e5e4d5a03e6b21f8d7a4064140d5c45eeab832b
Alex <rabbit_1399@icloud.com>
2023-09-28 17:28:37 +0300

    fix: dirty version

--- wunderkafka/librdkafka.py
+++ wunderkafka/librdkafka.py
@@ -2,4 +2,6 @@ from confluent_kafka import libversion
 
 _triplet, _ = libversion()
 
+if '-' in _triplet:
+    _triplet = _triplet.split('-')[0]
 __version__ = tuple(int(digit) for digit in _triplet.split('.'))
