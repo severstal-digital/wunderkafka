f9497081b95bc14dbce747a7329a6cd0a36bd226
Kirill Tribunsky <tribunsky.kir@yandex.ru>
2024-01-03 18:33:27 +0800

    fix: python 3.8 TypeError: 'type' object is not subscriptable

--- wunderkafka/serdes/avro/types.py
+++ wunderkafka/serdes/avro/types.py
@@ -2,4 +2,4 @@ from typing import Dict, List, Union, Any
 
 Field = Dict[str, Union[str, List[str]]]
 # Just what we get from `from fastavro import parse_schema`
-FastAvroParsedSchema = Union[str, list[Any], Dict[Any, Any]]
+FastAvroParsedSchema = Union[str, List[Any], Dict[Any, Any]]
