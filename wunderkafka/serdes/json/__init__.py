d45deebf175f01b3a863373d993c79a73c74bb14
Alex <rabbit_1399@icloud.com>
2024-08-15 21:52:31 +1000

    fix: update pydantic version for tests

--- wunderkafka/serdes/json/__init__.py
+++ wunderkafka/serdes/json/__init__.py
@@ -3,6 +3,6 @@ HAS_JSON_SCHEMA = True
 try:
     from confluent_kafka.schema_registry.json_schema import JSONDeserializer
 
-    __all__ = (JSONDeserializer,)
+    __all__ = ["JSONDeserializer", ]
 except ImportError:
     HAS_JSON_SCHEMA = False
