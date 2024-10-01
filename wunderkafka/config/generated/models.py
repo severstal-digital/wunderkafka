786b398d8931ad0c5ef7087c1f043934a7756dd8
alexotl777 <alexotl228@yandex.ru>
2024-10-01 21:41:15 +0300

    feat: edit choice of confluent versions

--- wunderkafka/config/generated/models.py
+++ wunderkafka/config/generated/models.py
@@ -5,27 +5,28 @@ from wunderkafka import librdkafka
 # mypy: disable-error-code="no-redef"
 
 
-if librdkafka.__version__ >= (1, 5, 0):
-    from wunderkafka.config.generated.confluent_1_5_0.models import *
-
-if librdkafka.__version__ >= (1, 6, 0):
-    from wunderkafka.config.generated.confluent_1_6_0.models import *
-
-if librdkafka.__version__ >= (1, 7, 0):
-    from wunderkafka.config.generated.confluent_1_7_0.models import *
-
-if librdkafka.__version__ >= (1, 8, 0):
-    from wunderkafka.config.generated.confluent_1_8_0.models import *
-
-if librdkafka.__version__ >= (1, 9, 0):
-    from wunderkafka.config.generated.confluent_1_9_0.models import *
-
-if librdkafka.__version__ >= (2, 0, 0):
-    from wunderkafka.config.generated.confluent_2_0_0.models import *
-
-if librdkafka.__version__ >= (2, 1, 0):
-    from wunderkafka.config.generated.confluent_2_1_0.models import *
-
 if librdkafka.__version__ >= (2, 2, 0):
     from wunderkafka.config.generated.confluent_2_2_0.models import *
 
+elif librdkafka.__version__ >= (2, 1, 0):
+    from wunderkafka.config.generated.confluent_2_1_0.models import *
+
+elif librdkafka.__version__ >= (2, 0, 0):
+    from wunderkafka.config.generated.confluent_2_0_0.models import *
+
+elif librdkafka.__version__ >= (1, 9, 0):
+    from wunderkafka.config.generated.confluent_1_9_0.models import *
+
+elif librdkafka.__version__ >= (1, 8, 0):
+    from wunderkafka.config.generated.confluent_1_8_0.models import *
+
+elif librdkafka.__version__ >= (1, 7, 0):
+    from wunderkafka.config.generated.confluent_1_7_0.models import *
+
+elif librdkafka.__version__ >= (1, 6, 0):
+    from wunderkafka.config.generated.confluent_1_6_0.models import *
+
+elif librdkafka.__version__ >= (1, 5, 0):
+    from wunderkafka.config.generated.confluent_1_5_0.models import *
+else:
+    from wunderkafka.config.generated.default.models import *
