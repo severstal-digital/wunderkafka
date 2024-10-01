92190eabe329d74a7c14ff5975408a70c883017b
alexotl777 <alexotl228@yandex.ru>
2024-10-01 14:34:11 +0300

    feat: custom ConInt

--- wunderkafka/config/generated/models.py
+++ wunderkafka/config/generated/models.py
@@ -7,7 +7,7 @@ from wunderkafka import librdkafka
 # I am not gonna to generate single type for every single range of conint/confloat.
 # https://github.com/samuelcolvin/pydantic/issues/156
 
-from typing import Callable, Optional
+from typing import Callable, Optional, Type
 
 from pydantic import conint, confloat
 from pydantic_settings import BaseSettings
@@ -16,9 +16,10 @@ from pydantic_settings import BaseSettings
 # Of course, it will fail with cimpl.KafkaException, but later, when Consumer/Producer are really initiated
 from wunderkafka.config.generated import enums
 
+ConInt: Type[int] = conint(ge=0, le=604800000)
 
 class RDKafkaConfig(BaseSettings):
-    api_version_fallback_ms: conint(ge=0, le=604800000) = 0
+    api_version_fallback_ms: ConInt = 0
     api_version_request: bool = True
     api_version_request_timeout_ms: conint(ge=1, le=300000) = 10000                           # type: ignore[valid-type]
     background_event_cb: Optional[Callable] = None
